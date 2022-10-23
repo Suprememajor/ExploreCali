93e753cb18ef7c871b31015460f60caa313176d0
Suprememajor <ndipnobertetta@gmail.com>
2022-10-23 23:16:26 +0100

    Create Repositories

---
+++ src/main/java/com/supremecorp/explorecali/repository/TourPackageRepository.java
@@ -0,0 +1,7 @@
+package com.supremecorp.explorecali.repository;
+
+import com.supremecorp.explorecali.domain.TourPackage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+}
---
+++ src/main/java/com/supremecorp/explorecali/repository/TourRepository.java
@@ -0,0 +1,7 @@
+package com.supremecorp.explorecali.repository;
+
+import com.supremecorp.explorecali.domain.Tour;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+}
