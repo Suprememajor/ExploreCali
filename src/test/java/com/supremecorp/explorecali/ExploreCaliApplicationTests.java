2653cc9d96177cdbf13bc7fc52095504a55b09fa
Suprememajor <ndipnobertetta@gmail.com>
2022-10-23 22:42:16 +0100

    Project init

---
+++ src/test/java/com/supremecorp/explorecali/ExploreCaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supremecorp.explorecali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreCaliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
